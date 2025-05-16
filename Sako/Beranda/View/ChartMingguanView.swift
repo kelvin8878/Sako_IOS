d48f3cf46a41d613dd5192df7d36e4fa75fb94bc
kelvin8878 <kelvinalexander3010@gmail.com>
2025-05-16 22:26:00 +0700

    finalize

--- Sako/Beranda/View/ChartMingguanView.swift
+++ Sako/Beranda/View/ChartMingguanView.swift
@@ -23,7 +23,7 @@ struct ChartMingguanView: View {
             .frame(height: 200)
             .padding(.top, 5)
             .chartYAxis {
-                AxisMarks(position: .leading, values: [0, 1000000, 2000000, 3000000, 4000000, 5000000])
+                AxisMarks(position: .leading, values: [0,500000, 1000000, 2000000, 3000000, 4000000, 5000000])
             }
             .contentShape(Rectangle()) // Membuat seluruh area chart dapat ditekan
 
